dd6da69330e289a7703f38fcd0f08e4fea5ebb02
Lilo002 <lisabasarab@gmail.com>
2023-12-04 18:07:59 +0300

    feat: done with DNS-stats (Mon, Dec  4, 2023  6:07:59 PM)

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let DNSObj = {};
+
+  for( let i = 0; i < domains.length; i += 1) {
+    let arr = domains[i].split('.');
+    for (let x = arr.length; x >= 0; x -= 1) {
+      let DNSArr = [];
+      let counter = x;
+
+      while(counter < arr.length) {
+        DNSArr.push(`.${arr[counter]}`);
+        counter ++;
+      }
+
+      let DNSStr = DNSArr.reverse().join('');
+
+      if (DNSObj.hasOwnProperty(DNSStr) && DNSStr) {
+        DNSObj[DNSStr] += 1;
+      } else if (DNSStr) {
+        DNSObj[DNSStr] = 1;
+      }
+    }
+  }
+  return DNSObj
 }
 
 module.exports = {
--- src/test.js
+++ src/test.js
@@ -1,9 +1,27 @@
-function isMAC48Address(n) {
-  let arr = n.split('-');
-  console.log(arr);
-  return !arr.some((item, index) => isNaN(parseInt(item, 16)));
+function getDNSStats(domains) {
+  let DNSObj = {};
+
+  for( let i = 0; i < domains.length; i += 1) {
+    let arr = domains[i].split('.');
+    for (let x = arr.length; x >= 0; x -= 1) {
+      let DNSArr = [];
+      let counter = x;
+
+      while(counter < arr.length) {
+        DNSArr.push(`.${arr[counter]}`);
+        counter ++;
+      }
+
+      let DNSStr = DNSArr.reverse().join('');
+
+      if (DNSObj.hasOwnProperty(DNSStr) && DNSStr) {
+        DNSObj[DNSStr] += 1;
+      } else if (DNSStr) {
+        DNSObj[DNSStr] = 1;
+      }
+    }
+  }
+  return DNSObj
 }
 
-  console.log(isMAC48Address('not a MAC-48 address'))
-
-  console.log(parseInt('Z1', 16))
+  console.log(getDNSStats(['epam.com', 'info.epam.com']))
