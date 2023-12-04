7b560607458635c4dbadb51db6fa3378614e4a69
Lilo002 <lisabasarab@gmail.com>
2023-12-04 12:44:27 +0300

    feat: done count-cats task (Mon, Dec  4, 2023 12:44:27 PM)

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cats = 0;
+
+  matrix.forEach((item, i) => {
+    item.forEach((item, i) => {
+      if (item === '^^') {
+        cats += 1;
+      }
+    })
+  })
+  return cats;
 }
 
 module.exports = {
