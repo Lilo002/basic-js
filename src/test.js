4bcb4223285757b5d0da0d2ab443ff2c16dd6327
Lilo002 <lisabasarab@gmail.com>
2023-12-04 14:33:24 +0300

    feat: done mac-address task (Mon, Dec  4, 2023  2:33:24 PM)

--- src/test.js
+++ src/test.js
@@ -1,21 +1,9 @@
-function getMatrixElementsSum(matrix) {
-  let sum = 0;
-  for (let i = 0; i < matrix.length; i += 1) {
-    for (let x = 0; x < matrix[i].length; x += 1) {
-      console.log(`falese/true`, matrix[i][x - 1] === 0)
-      console.log(`number`, matrix[i][x - 1])
-      if (matrix[i - 1]?.[x] === 0) {
-
-      } else {
-        sum += matrix[i][x];
-      }
-    }
-  }
-  return sum;
+function isMAC48Address(n) {
+  let arr = n.split('-');
+  console.log(arr);
+  return !arr.some((item, index) => isNaN(parseInt(item, 16)));
 }
 
-  console.log(getMatrixElementsSum([
-    [0, 1, 1, 2],
-    [0, 5, 0, 0],
-    [2, 0, 3, 3],
-  ]))
+  console.log(isMAC48Address('not a MAC-48 address'))
+
+  console.log(parseInt('Z1', 16))
