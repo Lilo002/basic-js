7d4a8b521e870f406b81a5221cd4640f0ca920d3
Lilo002 <lisabasarab@gmail.com>
2023-12-04 13:15:19 +0300

    feat: done sum-degits task (Mon, Dec  4, 2023  1:15:19 PM)

--- src/test.js
+++ src/test.js
@@ -1,29 +1,9 @@
-function encodeLine(str) {
-  let arr = str.split('');
-  let newStr = '';
-while(arr.length) {
-  for (let x = 0; x < arr.length; x += 1) {
-    let counter = 1;
-    console.log(arr);
-    for (let i = 0; i < arr.length; i += 1) {
-      console.log(arr[i])
-      if (x !== i) {
-        if (arr[x] !== arr[i]) {
-          break;
-        }
-        counter += 1;
-      }
-    }
-    newStr += `${counter > 1 ? counter : ''}${arr[0]}`;
-    arr.splice(0, counter);
-      if (arr.length) {
-        x = 0;
-        i = 0;
-    }
+function getSumOfDigits(n) {
+  while (n > 9) {
+    let arr = n.toString().split('');
+    n = arr.reduce((acc, cur) => +acc + +cur);  
   }
-}
-  
-  return newStr;
+  return n;
 }
 
-  console.log(encodeLine('xyz'))
+  console.log(getSumOfDigits(99))
