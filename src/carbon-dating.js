e94e348709779bce086946252d382b79e11bb69a
Lilo002 <lisabasarab@gmail.com>
2023-12-04 12:42:24 +0300

    feat: done carbon-dating task (Mon, Dec  4, 2023 12:42:24 PM)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
 
+  let number = +sampleActivity;
+  if (number <= 0 || typeof number !== 'number' || number > 2023 || isNaN(number)) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+
+  let years = Math.ceil(Math.log(MODERN_ACTIVITY / number) / k);
+  if (years <= 0) {
+    return false;
+  }
+  return years;
+}
 module.exports = {
   dateSample
 };
