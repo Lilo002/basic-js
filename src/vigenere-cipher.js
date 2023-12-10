3f08f1cdeb75d478bc9bebf8bc38201f3c95218a
Lilo002 <lisabasarab@gmail.com>
2023-12-10 14:02:23 +0300

    feat: done vigenere task (Sun, Dec 10, 2023  2:02:23 PM)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,99 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolean = true) {
+    this.boolean = boolean;
+    this.arr = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z'
+    ];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(mes, key) {
+    if (arguments.length <= 1 || !mes || !key) {
+      throw new Error(`Incorrect arguments!`);
+    }
+    let upperCaseKey = key.toUpperCase();
+    let encodeStr = '';
+    let curIndex = 0;
+
+    for (let i = 0; i < mes.length; i += 1) {
+      if (this.arr.includes(mes[i].toUpperCase())) {
+        let index = 0;
+        index += this.arr.findIndex((item) => item === mes[i].toUpperCase());
+        index += this.arr.findIndex((item) => item === upperCaseKey[curIndex]);
+        if (index > 25) {
+          index = index - 26;
+        }
+        curIndex += 1;
+        if (curIndex === upperCaseKey.length) {
+          curIndex = 0;
+        }
+        encodeStr += this.arr.at(index);
+      } else {
+        encodeStr += mes[i];
+      }
+    }
+    if (this.boolean) {
+      return encodeStr;
+    } else {
+      return encodeStr.split('').reverse().join('');
+    }
+    
+  }
+  decrypt(mes, key) {
+    if (arguments.length <= 1 || !mes || !key) {
+      throw new Error(`Incorrect arguments!`);
+    }
+    let upperCaseKey = key.toUpperCase();
+    let encodeStr = '';
+    let curIndex = 0;
+
+    for (let i = 0; i < mes.length; i += 1) {
+      if (this.arr.includes(mes[i].toUpperCase())) {
+        let index = 0;
+        index += this.arr.findIndex((item) => item === mes[i].toUpperCase());
+        index -= this.arr.findIndex((item) => item === upperCaseKey[curIndex]);
+        if (index < 0) {
+          index = index + 26;
+        }
+        curIndex += 1;
+        if (curIndex === upperCaseKey.length) {
+          curIndex = 0;
+        }
+        encodeStr += this.arr.at(index);
+      } else {
+        encodeStr += mes[i];
+      }
+    }
+    if (this.boolean) {
+      return encodeStr;
+    } else {
+      return encodeStr.split('').reverse().join('');
+    }
   }
 }
 
